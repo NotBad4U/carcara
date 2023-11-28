f71c2126f744760d96a0cea4e6b2501eb469f826
GuilhermeLLS <guiluizls@gmail.com>
2023-11-28 09:51:19 -0300

    feat: add bvult and bvadd rules

--- carcara/src/checker/rules/bitvectors.rs
+++ carcara/src/checker/rules/bitvectors.rs
@@ -22,6 +22,63 @@ fn build_term_vec(term: &Rc<Term>, size: usize, pool: &mut dyn TermPool) -> Vec<
     term
 }
 
+pub fn extract(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
+    assert_clause_len(conclusion, 1)?;
+    let (((_, left_j), left_x), right) =
+        match_term_err!((= ((_ extract i j) x) (bbterm ...)) = &conclusion[0])?;
+
+    let left_j = left_j.as_integer().unwrap();
+    let mut index = left_j;
+
+    if let Some((Operator::BvBbTerm, args)) = left_x.as_op() {
+        let mut index = index.to_usize().unwrap();
+        for arg in right {
+            assert_eq(&args[index], arg)?;
+            index += 1;
+        }
+        return Ok(());
+    }
+
+    for arg in right {
+        let expected_arg = Term::IndexedOp {
+            op: IndexedOperator::BvBitOf,
+            op_args: vec![Constant::Integer(index.clone())],
+            args: vec![left_x.clone()],
+        };
+        let new_arg = pool.add(expected_arg);
+        assert_eq(&new_arg, arg)?;
+        index += 1;
+    }
+    Ok(())
+}
+
+pub fn ult(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
+    assert_clause_len(conclusion, 1)?;
+    let ((x, y), res) = match_term_err!((= (bvult x y) res) = &conclusion[0])?;
+
+    let Sort::BitVec(size) = pool.sort(x).as_sort().cloned().unwrap() else {
+        unreachable!();
+    };
+
+    let size = size.to_usize().unwrap();
+
+    let x = build_term_vec(x, size, pool);
+    let y = build_term_vec(y, size, pool);
+
+    let mut expected_res = build_term!(pool, (and (not {x[0].clone()}) {y[0].clone()}));
+
+    for i in 1..size {
+        let new_res = build_term!(
+            pool,
+            (or (and (= {x[i].clone()} {y[i].clone()}) {expected_res.clone()})
+                (and (not {x[i].clone()}) {y[i].clone()}))
+        );
+        expected_res = new_res;
+    }
+
+    assert_eq(&expected_res, res)
+}
+
 pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
     assert_clause_len(conclusion, 1)?;
     let ((x, y), res) = match_term_err!((= (bvadd x y) res) = &conclusion[0])?;
@@ -39,18 +96,18 @@ pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 
     for i in 1..size {
         let carry_i = build_term!(
-          pool,
-          (or (and {x[i - 1].clone()} {y[i - 1].clone()}) (and (xor {x[i - 1].clone()} {y[i - 1].clone()}) {carries[i - 1].clone()}))
-      );
+            pool,
+            (or (and {x[i - 1].clone()} {y[i - 1].clone()}) (and (xor {x[i - 1].clone()} {y[i - 1].clone()}) {carries[i - 1].clone()}))
+        );
         carries.push(carry_i);
     }
 
     let res_args: Vec<_> = (0..size)
         .map(|i| {
             build_term!(
-          pool,
-          (xor (xor {x[i].clone()} {y[i].clone()}) {carries[i].clone()})
-      )
+            pool,
+            (xor (xor {x[i].clone()} {y[i].clone()}) {carries[i].clone()})
+        )
         })
         .collect();
 
@@ -58,13 +115,38 @@ pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 
     assert_eq(&expected_res, res)
 }
+<<<<<<< HEAD
+=======
+
+#[cfg(test)]
+
+>>>>>>> b94cebc (feat: add bvult and bvadd rules)
 mod tests {
+    #[test]
+    fn extract() {
+        test_cases! {
+            definitions = "
+            (declare-fun a () Bool)
+            (declare-fun zz () (_ BitVec 12))
+            (declare-fun xx () (_ BitVec 12))
+            ",
+            "Using extract with x and y as bbterms" {
+              "(step t2 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011))))) :rule bitblast_extract)": true,
+              "(step t3 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b111 #b011)) ((_ bit_of 1) (ite a #b111 #b011))))) :rule bitblast_extract)": false,
+              "(step t2 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011))))) :rule bitblast_extract)": false,
+              "(step t4 (cl (= ((_ extract 11 4) (bbterm ((_ bit_of 0) zz) ((_ bit_of 1) zz) ((_ bit_of 2) zz) ((_ bit_of 3) zz) ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz))) (bbterm ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz)))) :rule bitblast_extract)": true,
+              "(step t5 (cl (= ((_ extract 11 4) (bbterm ((_ bit_of 0) zz) ((_ bit_of 1) zz) ((_ bit_of 2) zz) ((_ bit_of 3) zz) ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz))) (bbterm ((_ bit_of 3) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz)))) :rule bitblast_extract)": false,
+              "(step t5 (cl (= ((_ extract 11 4) (bbterm ((_ bit_of 0) zz) ((_ bit_of 1) zz) ((_ bit_of 2) zz) ((_ bit_of 3) zz) ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz))) (bbterm ((_ bit_of 4) xx) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz)))) :rule bitblast_extract)": false,
+            }
+        }
+    }
+
     #[test]
     fn ult() {
         test_cases! {
             definitions = "
-                (declare-fun x4 () (_ BitVec 4))
-                (declare-fun y4 () (_ BitVec 4))
+            (declare-fun x4 () (_ BitVec 4))
+            (declare-fun y4 () (_ BitVec 4))
             ",
             "Using bvult with x and y as bitvectors" {
               "(step t3 (cl (= (bvult x4 y4) (or (= ((_ bit_of 3) x4) ((_ bit_of 2) y4)) ((_ bit_of 3) x4) ((_ bit_of 2) y4)))) :rule bitblast_ult)": false,
@@ -76,13 +158,12 @@ mod tests {
             }
         }
     }
-
     #[test]
     fn add() {
         test_cases! {
             definitions = "
-                (declare-fun x4 () (_ BitVec 4))
-                (declare-fun y4 () (_ BitVec 4))
+            (declare-fun x4 () (_ BitVec 4))
+            (declare-fun y4 () (_ BitVec 4))
             ",
             "Using bvadd with x and y as bitvectors" {
               "(step t3 (cl (= (bvadd x4 y4) (bbterm ((_ bit_of 0) x4) ((_ bit_of 1) y4) ((_ bit_of 2) x4) ((_ bit_of 3) y4)))) :rule bitblast_bvadd)": false,
